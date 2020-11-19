51225d94000f4adf3eb4ccf3130447778d33e57e
lexyhandi <lexihandi@gmail.com>
2020-11-19 16:24:57 +0700

    add check connection

--- app/src/main/java/com/android/wisataapp/MainActivity.kt
+++ app/src/main/java/com/android/wisataapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.android.wisataapp
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.android.wisataapp.adapter.WisataAdapter
 import com.android.wisataapp.model.ResponseServer
 import com.android.wisataapp.model.Wisata
@@ -18,30 +22,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ConfigNetwork.getRetrofit().getDataWisata().enqueue(object : Callback<ResponseServer> {
-            override fun onResponse(
-                call: Call<ResponseServer>,
-                response: Response<ResponseServer>
-            ) {
-                Log.d("response server", response.message())
+        if (isConnected()) {
+            ConfigNetwork.getRetrofit().getDataWisata().enqueue(object : Callback<ResponseServer> {
+                override fun onResponse(
+                    call: Call<ResponseServer>,
+                    response: Response<ResponseServer>
+                ) {
+                    Log.d("response server", response.message())
 
-                if (response.isSuccessful) {
-                    progressBar.visibility = View.GONE
-                    val status = response.body()?.status_code
+                    if (response.isSuccessful) {
+                        progressBar.visibility = View.GONE
+                        val status = response.body()?.status_code
 
-                    if (status == 200) {
-                        val data = response.body()?.data
+                        if (status == 200) {
+                            val data = response.body()?.data
 
-                        showData(data)
+                            showData(data)
+                        }
                     }
                 }
-            }
 
-            override fun onFailure(call: Call<ResponseServer>, t: Throwable) {
-                progressBar.visibility = View.GONE
-                Log.d("error server", t.message.toString())
-            }
-        })
+                override fun onFailure(call: Call<ResponseServer>, t: Throwable) {
+                    progressBar.visibility = View.GONE
+                    Log.d("error server", t.message.toString())
+                }
+            })
+        } else {
+            progressBar.visibility = View.GONE
+            Toast.makeText(this, "Jaringan internet bermasalah", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        val connect: ConnectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        return connect.activeNetworkInfo != null && connect.activeNetworkInfo!!.isConnected
     }
 
     private fun showData(data: ArrayList<Wisata>?) {
